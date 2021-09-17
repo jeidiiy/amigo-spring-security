961079281ca4a4434431dd8e8e13d4eeba552699
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-09-17 21:19:16 +0900

    feat: 어노테이션을 활용한 권한 부여
    config 클래스에 @EnableGlobalMethodSecurity(prePostEnable = true)를 설정해주어야 @PreAuthorize()가 적용됨

--- src/main/java/com/example/demo/student/StudentManagementController.java
+++ src/main/java/com/example/demo/student/StudentManagementController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -16,24 +17,28 @@ public class StudentManagementController {
     );
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents() {
         System.out.println("getAllStudents");
         return students;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student) {
         System.out.println("registerNewStudent");
         System.out.println(student);
     }
 
     @DeleteMapping("{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable Integer studentId) {
         System.out.println("deleteStudent");
         System.out.println(studentId);
     }
 
     @PutMapping("{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable Integer studentId, @RequestBody Student student) {
         System.out.println("updateStudent");
         System.out.println(String.format("%s %s", studentId, student));
